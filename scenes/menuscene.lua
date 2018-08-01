eefe6e6feebc45228c75545a60a00112a02a6ec0
DomDom <dom.dumont@gmail.com>
2018-08-01 15:33:47 +0200

    Choose color

--- scenes/menuscene.lua
+++ scenes/menuscene.lua
@@ -8,6 +8,7 @@ local C_ROW_1_Y = 200
 local C_ROW_2_Y = 300
 
 function MenuScene:initialize(sweetness)
+    self.possibleColors = {"red", "yellow", "green", "blue", "purple", "orange"}
 end
 
 function MenuScene:on_scene_enter()
@@ -34,15 +35,50 @@ function MenuScene:on_scene_enter()
         print("pouet pouet 2")
         TheSceneManager:push_scene(require("scenes/gamescene"))
     end
+
+    self.changeColorButton = TheGUI:create_button("Change Color", 200, 500, 150, 50)
+    self.changeColorButton.onClicked = function()
+        self.currentColor = self.currentColor + 1
+        if self.currentColor > 6 then
+            self.currentColor = 1
+        end
+    end
+
+    self.currentColor = 1
 end
 
 function MenuScene:draw()
     Scene.draw(self) -- call base method
+    love.graphics.push("all")
+    self:setColor(self.possibleColors[self.currentColor])
+    love.graphics.rectangle("fill", 400, 450, 100, 100, 4)
+    love.graphics.pop()
 end
 
 function MenuScene:keypressed(key)
     if key == "escape" then
         love.event.push("quit")
+    end
+end
+
+function MenuScene:setColor(colorName)
+    print(colorName)
+    if colorName == "red" then
+        love.graphics.setColor(1, 0, 0, 1)
+    elseif colorName == "green" then
+        love.graphics.setColor(0, 1, 0, 1)
+    elseif colorName == "blue" then
+        love.graphics.setColor(0, 0, 1, 1)
+    elseif colorName == "orange" then
+        love.graphics.setColor(1, 0.65, 0, 1)
+    elseif colorName == "yellow" then
+        love.graphics.setColor(1, 1, 0, 1)
+    elseif colorName == "purple" then
+        love.graphics.setColor(0.93, 0.50, 0.93, 1)
+    elseif colorName == "white" then
+        love.graphics.setColor(1, 1, 1, 1)
+    else
+        love.graphics.setColor(1, 1, 1, 1)
     end
 end
 
